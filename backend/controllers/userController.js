afe1900c1197fc2930045278c1cd06fc93233d62
Aveelash <aveelash.hota04@gmail.com>
2025-07-26 19:51:34 +0530

    Completed admin functionality with the user part and also completed reviews part both backend and frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -121,28 +121,65 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // GET /api/users
 // Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get users");
+  const users = await User.find({});
+  res.status(200).json(users);
 });
 
 // Delete users
 // DELETE /api/users/:id
 // Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin user");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.status(201).json({ message: "User deleted Successfully" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // Get Users by id
 // GET /api/users/:id
 // Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    throw new Error("User not found");
+  }
 });
 
 // update user
 // PUT /api/users/:id
 // Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updateUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
